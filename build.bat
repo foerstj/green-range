3910d1595b13c9f7a3d182fd20424a332175db2d
Johannes Förstner <johannes@famfoerstner.de>
2022-01-29 10:17:46 +0100

    Add guard npcs and a starter shop

--- build.bat
+++ build.bat
@@ -20,6 +20,7 @@ if %errorlevel% neq 0 pause
 
 :: Compile resource file
 rmdir /S /Q "%tmp%\Bits"
+robocopy "%doc_dsloa%\Bits\world\contentdb\templates\%map%" "%tmp%\Bits\world\contentdb\templates\%map%" /E
 robocopy "%doc_dsloa%\Bits\world\global\moods\%map%" "%tmp%\Bits\world\global\moods\%map%" /E
 %tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\DSLOA\%map_cs%.dsres" -copyright "CC-BY-SA 2022" -title "%map_cs%" -author "Johannes Förstner"
 if %errorlevel% neq 0 pause
