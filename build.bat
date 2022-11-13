fcff41df53831b3c9b9761c463e85b6db48dee0c
Johannes Förstner <johannes@famfoerstner.de>
2022-11-13 20:36:12 +0100

    Build exit & stitch & add tx

--- build.bat
+++ build.bat
@@ -32,12 +32,12 @@ popd
 
 :: Compile map file
 rmdir /S /Q "%tmp%\Bits"
-::robocopy "%doc_dsloa%\Bits\world\maps\%map%" "%tmp%\Bits\world\maps\%map%" /E
-robocopy "%doc_dsloa%\Bits\world\maps\%map%" "%tmp%\Bits\world\maps\%map%" /E /xd regions
-robocopy "%doc_dsloa%\Bits\world\maps\%map%\regions\dream-cave-a" "%tmp%\Bits\world\maps\%map%\regions\dream-cave-a" /E
-robocopy "%doc_dsloa%\Bits\world\maps\%map%\regions\dream-cave-b" "%tmp%\Bits\world\maps\%map%\regions\dream-cave-b" /E
-robocopy "%doc_dsloa%\Bits\world\maps\%map%\regions\dream-cave-bottom" "%tmp%\Bits\world\maps\%map%\regions\dream-cave-bottom" /E
-robocopy "%doc_dsloa%\Bits\world\maps\%map%\regions\mountains-x11z08" "%tmp%\Bits\world\maps\%map%\regions\mountains-x11z08" /E
+robocopy "%doc_dsloa%\Bits\world\maps\%map%" "%tmp%\Bits\world\maps\%map%" /E
+::robocopy "%doc_dsloa%\Bits\world\maps\%map%" "%tmp%\Bits\world\maps\%map%" /E /xd regions
+::robocopy "%doc_dsloa%\Bits\world\maps\%map%\regions\dream-cave-a" "%tmp%\Bits\world\maps\%map%\regions\dream-cave-a" /E
+::robocopy "%doc_dsloa%\Bits\world\maps\%map%\regions\dream-cave-b" "%tmp%\Bits\world\maps\%map%\regions\dream-cave-b" /E
+::robocopy "%doc_dsloa%\Bits\world\maps\%map%\regions\dream-cave-bottom" "%tmp%\Bits\world\maps\%map%\regions\dream-cave-bottom" /E
+::robocopy "%doc_dsloa%\Bits\world\maps\%map%\regions\mountains-x11z08" "%tmp%\Bits\world\maps\%map%\regions\mountains-x11z08" /E
 pushd %gaspy%
 venv\Scripts\python -m build.fix_start_positions_required_levels %map% "%tmp%\Bits"
 if %errorlevel% neq 0 pause
