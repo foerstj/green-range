f6b7dc6f184e08974ace93b1c110f1d6112cd54f
Johannes Förstner <johannes@famfoerstner.de>
2022-12-22 20:14:54 +0100

    Reset start-pos & build-script

--- build.bat
+++ build.bat
@@ -34,11 +34,11 @@ popd
 
 :: Compile map file
 rmdir /S /Q "%tmp%\Bits"
-::robocopy "%doc_dsloa%\Bits\world\maps\%map%" "%tmp%\Bits\world\maps\%map%" /E
-robocopy "%doc_dsloa%\Bits\world\maps\%map%" "%tmp%\Bits\world\maps\%map%" /E /xd regions
-for %%r in (glacier-cavern-north glacier-cavern-river glacier-cavern-bridge glacier-cavern-south dripstone-cavern-west dripstone-cavern-east) do (
-  robocopy "%doc_dsloa%\Bits\world\maps\%map%\regions\%%r" "%tmp%\Bits\world\maps\%map%\regions\%%r" /E
-)
+robocopy "%doc_dsloa%\Bits\world\maps\%map%" "%tmp%\Bits\world\maps\%map%" /E
+::robocopy "%doc_dsloa%\Bits\world\maps\%map%" "%tmp%\Bits\world\maps\%map%" /E /xd regions
+::for %%r in (glacier-cavern-north glacier-cavern-river glacier-cavern-bridge glacier-cavern-south dripstone-cavern-west dripstone-cavern-east) do (
+::  robocopy "%doc_dsloa%\Bits\world\maps\%map%\regions\%%r" "%tmp%\Bits\world\maps\%map%\regions\%%r" /E
+::)
 pushd %gaspy%
 venv\Scripts\python -m build.fix_start_positions_required_levels %map% "%tmp%\Bits"
 if %errorlevel% neq 0 pause
