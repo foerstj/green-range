d8e1b3bdb958f687530d37352014a91ca306f909
Johannes Förstner <johannes@famfoerstner.de>
2023-01-05 17:32:36 +0100

    Stitch, Startpos, build.bat

--- build.bat
+++ build.bat
@@ -34,11 +34,11 @@ popd
 
 :: Compile map file
 rmdir /S /Q "%tmp%\Bits"
-::robocopy "%doc_dsloa%\Bits\world\maps\%map%" "%tmp%\Bits\world\maps\%map%" /E
-robocopy "%doc_dsloa%\Bits\world\maps\%map%" "%tmp%\Bits\world\maps\%map%" /E /xd regions
-for %%r in (glacier-shelter glacier-cavern-river glacier-cavern-north glacier-cavern-bridge dripstone-cavern-west dripstone-cavern-east) do (
-  robocopy "%doc_dsloa%\Bits\world\maps\%map%\regions\%%r" "%tmp%\Bits\world\maps\%map%\regions\%%r" /E
-)
+robocopy "%doc_dsloa%\Bits\world\maps\%map%" "%tmp%\Bits\world\maps\%map%" /E
+::robocopy "%doc_dsloa%\Bits\world\maps\%map%" "%tmp%\Bits\world\maps\%map%" /E /xd regions
+::for %%r in (glacier-shelter glacier-cavern-river glacier-cavern-north glacier-cavern-bridge dripstone-cavern-west dripstone-cavern-east) do (
+::  robocopy "%doc_dsloa%\Bits\world\maps\%map%\regions\%%r" "%tmp%\Bits\world\maps\%map%\regions\%%r" /E
+::)
 pushd %gaspy%
 venv\Scripts\python -m build.fix_start_positions_required_levels %map% "%tmp%\Bits"
 if %errorlevel% neq 0 pause
@@ -48,7 +48,8 @@ if %errorlevel% neq 0 pause
 
 :: Compile main resource file
 rmdir /S /Q "%tmp%\Bits"
-robocopy "%doc_dsloa%\Bits\art" "%tmp%\Bits\art" /E /xf .gitignore /xf *.psd
+robocopy "%doc_dsloa%\Bits\art" "%tmp%\Bits\art" /E /xd terrain /xf .gitignore /xf *.psd /xf mdkey.nnk /xf namingkey.nnk /xf namingkey101.nnk
+robocopy "%doc_dsloa%\Bits\art\terrain\generic\droog" "%tmp%\Bits\art\terrain\generic\droog" /E /xd terrain /xf .gitignore /xf *.psd
 robocopy "%doc_dsloa%\Bits\sound\effects" "%tmp%\Bits\sound\effects" /E
 robocopy "%doc_dsloa%\Bits\world\ai\jobs\%res%" "%tmp%\Bits\world\ai\jobs\%res%" /E
 robocopy "%doc_dsloa%\Bits\world\contentdb\components" "%tmp%\Bits\world\contentdb\components" /E
