262cc5d40a186c4ed4eaa2571e3e21f61d09db0f
Johannes Förstner <johannes@famfoerstner.de>
2024-03-28 13:08:21 +0100

    Generate V&E world levels

--- build.bat
+++ build.bat
@@ -53,6 +53,12 @@ if "%mode%"=="light" (
 pushd %gaspy%
 venv\Scripts\python -m build.fix_start_positions_required_levels %map% "%tmp%\Bits"
 if %errorlevel% neq 0 pause
+setlocal EnableDelayedExpansion
+if "%mode%"=="release" (
+  venv\Scripts\python -m build.add_world_levels %map% "%tmp%\Bits" "%doc_dsloa%\Bits"
+  if !errorlevel! neq 0 pause
+)
+endlocal
 popd
 %tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\DSLOA\%map_cs%.dsmap" -copyright "%copyright%" -title "%title%" -author "%author%"
 if %errorlevel% neq 0 pause
